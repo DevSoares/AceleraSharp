847ad328dc57d72f8f24c6016900a54a5fa673b6
Luiz Soares <dev.soares@outlook.com>
2020-01-19 22:19:54 -0300

    Added console app 'AssistenteDoProfessor' and 'CalculadoraSimples'

---
+++ AssistenteDoProfessor/AssistenteDoProfessor/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistenteDoProfessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "Bom dia professor!";
+            string mensagemTecleAlgo = "Pressione uma tecla para sair";
+            List<double> notas = new List<double>();
+            string sairMenu = string.Empty;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(title);
+                try
+                {
+                    Console.WriteLine($"Digite a nota Nº {notas.Count + 1} do aluno");
+                    notas.Add(double.Parse(Console.ReadLine()));
+                    Console.WriteLine("Todas as notas Foram Lidas?");
+                    sairMenu = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\n{mensagemTecleAlgo}");
+                    Console.ReadKey();
+                }
+
+            } while (sairMenu != "s");
+
+            double media = CalcularMedia(notas);
+            Console.Clear();
+            Console.WriteLine($"Média do 'Joãozinho' é: {media}");
+            double valorMax = double.Parse(ConfigurationManager.AppSettings["valorMax"]);
+            double valorMedio = double.Parse(ConfigurationManager.AppSettings["valorMedio"]);
+            double valorMin = double.Parse(ConfigurationManager.AppSettings["valorMin"]);
+
+            if (media >= valorMax)
+            {
+                Console.WriteLine($"Passou 'Joãozinho' PORRA\n{mensagemTecleAlgo}");
+            }
+            else if (media < valorMedio && media > valorMin)
+            {
+                Console.WriteLine($"Caraio 'Joãozinho' que vacilo\n{mensagemTecleAlgo}");
+            }
+            else
+            {
+                Console.WriteLine($"'Joãozinho' ano que vem tu consegue\n{mensagemTecleAlgo}");
+            }
+            Console.ReadKey();
+        }
+
+        public static double CalcularMedia(List<double> notas)
+        {
+            return notas.Sum() / notas.Count;
+        }
+    }
+}
---
+++ CalculadoraSimples/CalculadoraSimples/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool trava1 = false;
+            int valor1 = 0;
+            int valor2 = 0;
+            do
+            {
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("Digite Valor numero 1: ");
+                    valor1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite Valor numero 2: ");
+                    valor2 = int.Parse(Console.ReadLine());
+
+                    trava1 = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Pressione qualquer tecla");
+                    Console.ReadKey();
+
+                }
+
+            } while (trava1 == false);
+
+            int opcao = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@"Escolha uma das opções
+                    1 - Somar
+                    2 - Subtrair Segundo do Primeiro
+                    3 - Subtrair Primeiro do Segundo
+                    4 - Multiplicar
+                    5 - Dividir Primeiro pelo Segundo
+                    6 - Dividir Segundo pelo Primeiro
+                    7 - Potenciar Primeiro pelo Segundo
+                    8 - Potenciar Segundo pelo Primeiro
+                    9 - Sair");
+
+                try
+                {
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine(SomaDoisValores(valor1, valor2));
+                        break;
+
+                    case 2:
+                        Console.WriteLine(SubtraiSegundoDoPrimeiro(valor1, valor2));
+                        break;
+
+                    case 3:
+                        Console.WriteLine(SubtraiPrimeiroDoSegundo(valor1, valor2));
+                        break;
+
+                    case 4:
+                        Console.WriteLine(Multiplicar(valor1, valor2));
+                        break;
+
+                    case 5:
+                        Console.WriteLine(DividirPrimeiroPeloSegundo(valor1, valor2));
+                        break;
+
+                    case 6:
+                        Console.WriteLine(DividirSegundoPeloPrimeiro(valor1, valor2));
+                        break;
+
+                    case 7:
+                        Console.WriteLine(PotenciarPrimeiroSegundo(valor1, valor2));
+                        break;
+
+                    case 8:
+                        Console.WriteLine(PotenciarSegundoPrimeiro(valor1, valor2));
+                        break;
+
+                    case 9:
+                        opcao = 9;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção não disponível.");
+                        break;
+                }
+                Console.ReadKey();
+
+            } while (opcao != 9);
+        }
+
+        public static int SomaDoisValores(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+
+        public static int SubtraiPrimeiroDoSegundo(int param1, int param2)
+        {
+            int valorRetorno = param2 - param1;
+            return valorRetorno;
+        }
+
+        public static int SubtraiSegundoDoPrimeiro(int param1, int param2)
+        {
+            int valorRetorno = param1 - param2;
+            return valorRetorno;
+        }
+
+        public static float Multiplicar(int param1, int param2)
+        {
+            return param1 * param2;
+        }
+
+        public static float DividirPrimeiroPeloSegundo(float param1, float param2)
+        {
+            float valorRetorno = param1 / param2;
+            return valorRetorno;
+        }
+
+        public static float DividirSegundoPeloPrimeiro(float param1, float param2)
+        {
+            float valorRetorno = param2 / param1;
+            return valorRetorno;
+        }
+
+        public static double PotenciarPrimeiroSegundo(float param1, float param2)
+        {
+            return Math.Pow(param1, param2);
+        }
+
+        public static double PotenciarSegundoPrimeiro(float param1, float param2)
+        {
+            return Math.Pow(param2, param1);
+        }
+
+    }
+}
